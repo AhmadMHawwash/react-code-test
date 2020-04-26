31bf78f9897d9cc4f749685ae556d6e4430136e8
Ahmad Hawwash <ahmad.m.hawwash@gmail.com>
2020-04-26 12:21:34 +0300

    more styling formatting

--- src/Components/Pages/Users/Components/HeaderRenderer.tsx
+++ src/Components/Pages/Users/Components/HeaderRenderer.tsx
@@ -1,22 +1,8 @@
 import React, { FunctionComponent } from "react";
+import "../styles.scss";
 
-interface HeaderRendererProps {
-
-}
+interface HeaderRendererProps {}
 
 export const HeaderRenderer: FunctionComponent<HeaderRendererProps> = () => {
-  return (
-    <div
-      style={{
-        height: 75,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        background: "rgb(241 241 241)",
-        fontWeight: 600,
-      }}
-    >
-      Users
-    </div>
-  );
+  return <div className="UsersPage--Header">Users</div>;
 };
